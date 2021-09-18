4254957b9615d71f95af0be97d6b1ee0655cf9e4
SuryaParbat <SuryaParbat@.github.com>
2021-09-19 03:35:44 +0530

    setup done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="its a air Temperature predictor",
+    author="SuryaParbat",
+    packages=find_packages(),
+    license="MIT"
+)
